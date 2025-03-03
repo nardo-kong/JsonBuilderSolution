15041eeec6bcca61923316ed228b6253d46ab60e
Nardo <nardokong@gmail.com>
2025-03-03 17:39:33 +0800

    add default value

--- JsonBuilder.Core/Models/Messages/PickConfirmMessage.cs
+++ JsonBuilder.Core/Models/Messages/PickConfirmMessage.cs
@@ -24,19 +24,19 @@ namespace JsonBuilder.Core.Models.Messages
         public string BoxNumber { get; set; } = "";
 
         [JsonProperty(PropertyName = "divert_time", Order = 2)]
-        public string DivertTime { get; set; } = "";
+        public string DivertTime { get; set; } = "20250303171152";
 
         [JsonProperty(PropertyName = "weight", Order = 3)]
-        public string Weight { get; set; } = "";
+        public string Weight { get; set; } = "6000";
 
         [JsonProperty(PropertyName = "transferid", Order = 4)]
-        public string TransferId { get; set; } = "";
+        public string TransferId { get; set; } = "26955983";
 
         [JsonProperty(PropertyName = "customer_code", Order = 5)]
         public string CustomerCode { get; set; } = "";
 
         [JsonProperty(PropertyName = "uuid", Order = 6)]
-        public string Uuid { get; set; } = "";
+        public string Uuid { get; set; } = "f1e75a54-46e7-4e8f-87d1-cffa2753b7ae";
 
         [JsonProperty(PropertyName = "host_order_id", Order = 7)]
         public string HostOrderId { get; set; } = "";
